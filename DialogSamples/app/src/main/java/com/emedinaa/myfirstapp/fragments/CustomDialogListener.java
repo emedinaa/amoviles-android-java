2a930fbebcc2c78917f991ad1ef19a7434fc0d89
Eduardo Medina <emedinaa@gmail.com>
2018-08-28 18:07:11 -0500

    Added Dialog samples

---
+++ DialogSamples/app/src/main/java/com/emedinaa/myfirstapp/fragments/CustomDialogListener.java
@@ -0,0 +1,11 @@
+package com.emedinaa.myfirstapp.fragments;
+
+/**
+ * Created by em on 24/06/16.
+ */
+public interface CustomDialogListener {
+
+    void onAction(Object object);
+    void onDialogPositive(Object object);
+    void onDialogNegative(Object object);
+}
---
+++ DialogSamples/app/src/main/java/com/emedinaa/myfirstapp/fragments/SelectionDialog.java
@@ -0,0 +1,36 @@
+package com.emedinaa.myfirstapp.fragments;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+import com.emedinaa.myfirstapp.R;
+
+
+/**
+ * Created by eduardomedina on 31/01/18.
+ */
+
+public class SelectionDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.pick_color)
+                .setItems(R.array.colors_array, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // The 'which' argument contains the index position
+                        // of the selected item
+                        log("item "+which);
+                    }
+                });
+        return builder.create();
+    }
+
+    private void log(String message){
+        Log.d("CONSOLE",message);
+    }
+}
