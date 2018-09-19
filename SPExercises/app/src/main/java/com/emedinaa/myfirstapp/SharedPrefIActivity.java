beac47cc376965645169a8aaa1ff9c4cabbcb217
Eduardo Medina <emedinaa@gmail.com>
2018-09-18 21:58:36 -0500

    Updated sp exercises

--- SPExercises/app/src/main/java/com/emedinaa/myfirstapp/SharedPrefIActivity.java
+++ SPExercises/app/src/main/java/com/emedinaa/myfirstapp/SharedPrefIActivity.java
@@ -1,7 +1,6 @@
 package com.emedinaa.myfirstapp;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,28 +8,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.emedinaa.myfirstapp.storage.PreferencesHelper;
+
 public class SharedPrefIActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button button,button2,button3,button4;
     private Button button5,button6;
 
     private String currentColor=null;
-    private SharedPreferences sharedPreferences;
+    //private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shared_pref_01);
 
-        sharedPreferences= getSharedPreferences(
+        /*sharedPreferences= getSharedPreferences(
                 "com.amoviles.spexercises",
-                Context.MODE_PRIVATE);
+                Context.MODE_PRIVATE);*/
         ui();
         checkColor();
     }
 
     private void checkColor(){
-        currentColor= sharedPreferences.getString("COLOR",null);
+        //currentColor= sharedPreferences.getString("COLOR",null);
+        currentColor= PreferencesHelper.getColor(this);
         if(currentColor!=null){
             int colorValue= Color.parseColor(currentColor);
             findViewById(R.id.frameLayout).setBackgroundColor(colorValue);
@@ -79,6 +81,7 @@ public class SharedPrefIActivity extends AppCompatActivity implements View.OnCli
                 break;
             case R.id.button6:
                 reset();
+                goToLogIn();
                 break;
         }
     }
@@ -88,24 +91,34 @@ public class SharedPrefIActivity extends AppCompatActivity implements View.OnCli
         int colorValue= Color.parseColor(color);
         findViewById(R.id.frameLayout).setBackgroundColor(colorValue);
 
-        Toast.makeText(this,"currenColor "+currentColor,
-                Toast.LENGTH_SHORT).show();
+        /*Toast.makeText(this,"currenColor "+currentColor,
+                Toast.LENGTH_SHORT).show();*/
     }
 
     private void save(){
         if(currentColor==null)return;
-        SharedPreferences.Editor editor=sharedPreferences.edit();
+        /*SharedPreferences.Editor editor=sharedPreferences.edit();
         editor.putString("COLOR", currentColor);
-        editor.apply();
+        editor.apply();*/
+        PreferencesHelper.saveColor(currentColor,this);
+
+        Toast.makeText(this,"Saved color "+currentColor,
+                Toast.LENGTH_SHORT).show();
     }
 
     private void reset(){
         currentColor=null;
         int colorValue= Color.parseColor("#d4d4d4");
         findViewById(R.id.frameLayout).setBackgroundColor(colorValue);
-        SharedPreferences.Editor editor=sharedPreferences.edit();
+        /*SharedPreferences.Editor editor=sharedPreferences.edit();
         editor.clear();
         //editor.remove("COLOR");
-        editor.apply();
+        editor.apply();*/
+        PreferencesHelper.clearSession(this);
+    }
+
+    private void goToLogIn(){
+        startActivity(new Intent(this,LogInActivity.class));
+        finish();
     }
 }
--- SPExercises/app/src/main/java/com/emedinaa/myfirstapp/storage/PreferencesHelper.java
+++ SPExercises/app/src/main/java/com/emedinaa/myfirstapp/storage/PreferencesHelper.java
@@ -2,6 +2,7 @@
 package com.emedinaa.myfirstapp.storage;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 
 
 public class PreferencesHelper {
@@ -9,47 +10,64 @@ public class PreferencesHelper {
     private static final String MYNOTES_PREFERENCES = "com.amoviles.sharedpref";
     private static final String PREFERENCES_USERNAME = MYNOTES_PREFERENCES + ".username";
     private static final String PREFERENCES_PASSWORD = MYNOTES_PREFERENCES + ".password";
+    private static final String PREFERENCES_COLOR = MYNOTES_PREFERENCES + ".color";
     private static final String PRODUCT_ID=MYNOTES_PREFERENCES+".productId";
 
     private PreferencesHelper() {
         //no instance
     }
 
-    public static void signOut(Context context) {
+    public static void signOut(final Context context) {
         SharedPreferences.Editor editor = getEditor(context);
         editor.remove(PREFERENCES_USERNAME);
         editor.remove(PREFERENCES_PASSWORD);
         editor.apply();
     }
 
-    public static void saveSession(Context context, String username, String password)
-    {
+    public static void clearSession(final Context context){
         SharedPreferences.Editor editor = getEditor(context);
-        editor.putString(PREFERENCES_USERNAME, username);
-        editor.putString(PREFERENCES_PASSWORD, password);
+        editor.clear();
         editor.apply();
     }
 
-    public static String getUserSession(Context context)
+    public static void saveSession(final Context context, String username)
+    {
+        SharedPreferences.Editor editor = getEditor(context);
+        editor.putString(PREFERENCES_USERNAME, username);
+        editor.apply();
+    }
+
+    public static String getUserSession(final Context context)
     {
         SharedPreferences sharedPreferences= getSharedPreferences(context);
         String username= sharedPreferences.getString(PREFERENCES_USERNAME,null);
-
         return username;
     }
 
-    public static boolean isSignedIn(Context context) {
-        final SharedPreferences preferences = getSharedPreferences(context);
-        return preferences.contains(PREFERENCES_USERNAME) &&
-                preferences.contains(PREFERENCES_PASSWORD);
+    public static void saveColor(String color,@NonNull final  Context context){
+        SharedPreferences.Editor editor = getEditor(context);
+        editor.putString(PREFERENCES_COLOR, color);
+        editor.apply();
     }
 
-    private static SharedPreferences.Editor getEditor(Context context) {
+    public static String getColor(final Context context)
+    {
+        SharedPreferences sharedPreferences= getSharedPreferences(context);
+        String color= sharedPreferences.getString(PREFERENCES_COLOR,null);
+        return color;
+    }
+
+    public static boolean isSignedIn(final Context context) {
+        final SharedPreferences preferences = getSharedPreferences(context);
+        return preferences.contains(PREFERENCES_USERNAME);
+    }
+
+    private static SharedPreferences.Editor getEditor(final Context context) {
         SharedPreferences preferences = getSharedPreferences(context);
         return preferences.edit();
     }
 
-    private static SharedPreferences getSharedPreferences(Context context) {
+    private static SharedPreferences getSharedPreferences(final Context context) {
         return context.getSharedPreferences(MYNOTES_PREFERENCES, Context.MODE_PRIVATE);
     }
 }
