59f5c186d286427e80d0f8830c6b8d968159cffe
Eduardo Medina <emedinaa@gmail.com>
2018-09-24 19:15:03 -0500

    Added sample

---
+++ AppCompSamples/app/src/main/java/com/emedinaa/myfirstapp/services/SoundService.java
@@ -0,0 +1,49 @@
+package com.emedinaa.myfirstapp.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.emedinaa.myfirstapp.R;
+
+
+/**
+ * Created by eduardomedina on 8/02/17.
+ */
+
+public class SoundService extends Service {
+//public class SoundService extends IntentService {
+
+    private MediaPlayer mediaPlayer;
+
+    public SoundService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer= MediaPlayer.create(this, R.raw.shrek_song);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
